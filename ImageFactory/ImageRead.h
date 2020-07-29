90ecdd971199aa01911cdf3aef94b1e092471bb5
jlma <jlma.amo@qq.com>
2020-07-29 22:21:06 +0800

    Add multilple thread read

--- ImageFactory/ImageRead.h
+++ ImageFactory/ImageRead.h
@@ -8,8 +8,8 @@ public:
 
   virtual bool ReadImage(std::string file_path) = 0;
   virtual cv::Mat& GetImage() = 0;
-  ImageRead(const ImageRead&) = delete;
-  ImageRead& operator=(const ImageRead&) = delete;
+//  ImageRead(const ImageRead&) = delete;
+//  ImageRead& operator=(const ImageRead&) = delete;
 };
 
 
